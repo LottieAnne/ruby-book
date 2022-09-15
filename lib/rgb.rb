c311a8052c338291425e9b9e0acfc14497842319
cchaae <kkang_chung125@yahoo.co.jp>
2022-09-15 14:39:26 +0900

    refactor: sumメソッドを使用してリファクタリング

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,9 +1,7 @@
 def to_hex(r,g,b)
-    hex = '#'
-    [r, g, b].each do |n|
-        hex += n.to_s(16).rjust(2,'0')
+    [r, g, b].sum('#') do |n|
+       n.to_s(16).rjust(2,'0')
     end
-    hex
 end
 
 
